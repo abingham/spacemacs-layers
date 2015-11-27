abed706ee22b71fd97d5771db47f494974010f5c
Austin Bingham <austin@sixty-north.com>
2015-11-27 14:03:19 +0000

    Improved therapy executable detection.

--- abingham/funcs.el
+++ abingham/funcs.el
@@ -24,27 +24,36 @@ Replaces three keystroke sequence C-u 0 C-l."
   (delq nil (mapcar 'executable-find options)))
 
 (defun abingham-setup-pyflakes-executable (executable-name)
-  (if (executable-find executable-name)
+  (if (and executable-name
+           (executable-find executable-name))
       (setq flycheck-python-pyflakes-executable executable-name)
     (warn "No python flake8 executable found. Flycheck will be disabled for Python!")))
 
+(defun abingham--activate-python (version python-executables pyflakes-executables)
+  "Common python 2/3activation routine."
+  (message "Activating Python %s toolset." version)
+  (let ((pyex (first (abingham-find-executables python-executables))))
+    (if pyex
+        (set-variable 'python-shell-interpreter pyex)
+      (warn "No Python executable found!")))
+
+  ;; (ab-setup-therapy-tests)
+  (abingham-setup-pyflakes-executable
+   (car (abingham-find-executables pyflakes-executables))))
+
 (defun abingham-activate-python2 ()
   "Hook run when entering python2 environment."
-  (message "Activating Python 2 toolset.")
-  (set-variable 'python-shell-interpreter (first (abingham-find-executables '("ipython" "python"))))
-  (unless python-shell-interpreter (warn "No Python executable found!"))
-  ;; (ab-setup-therapy-tests)
-  (abingham-setup-pyflakes-executable "flake8"))
+  (abingham--activate-python
+   "2"
+   '("ipython" "python")
+   '("flake8")))
 
 (defun abingham-activate-python3 ()
   "Hook run when entering python3 environment."
-  (message "Activating Python 3 toolset.")
-  (set-variable 'python-shell-interpreter (first (abingham-find-executables '("ipython3" "python3" "python"))))
-  (unless python-shell-interpreter (warn "No Python executable found!"))
-  ;; (ab-setup-therapy-tests)
-  (abingham-setup-pyflakes-executable
-   (car (abingham-find-executables
-           '("flake8-3" "flake8")))))
+  (abingham--activate-python
+   "3"
+   '("ipython3" "python3" "python")
+   '("flake8-3" "flake8")))
 
 (defun abingham-pyvenv-hook ()
   "Hook for when we change virtual environments."
