443bdb6cd55e4048c080eccc7027e9f462b2e6dc
Austin Bingham <austin@sixty-north.com>
2015-12-28 08:58:37 +0000

    Added custom mode-line theme.

--- abingham/funcs.el
+++ abingham/funcs.el
@@ -77,3 +77,54 @@ Replaces three keystroke sequence C-u 0 C-l."
 
   ;; Ensure that all therapy hooks are run...
   (therapy-interpreter-changed))
+
+(defun abingham-mode-line-theme ()
+    "A custom mode-line theme.
+
+One goal here is to make sure that critical information (like
+line number) is visible as often as possible, so it's pulled all
+of the way to the left. Also, removed some of the sillier
+decorations.
+
+Based off of powerline-default-theme."
+    (interactive)
+    (setq-default mode-line-format
+                  '("%e"
+                    (:eval
+                     (let* ((active (powerline-selected-window-active))
+                            (mode-line (if active 'mode-line 'mode-line-inactive))
+                            (face1 (if active 'powerline-active1 'powerline-inactive1))
+                            (face2 (if active 'powerline-active2 'powerline-inactive2))
+                            (lhs (list (powerline-raw "%*" face1 'l)
+                                       (powerline-raw "%4l" face1 'l)
+                                       (powerline-raw ":" face1 'l)
+                                       (powerline-raw "%3c" face1 'r)
+                                       (powerline-raw " ")
+                                       (powerline-raw "%6p" nil 'r)
+
+                                       (when powerline-display-buffer-size
+                                         (powerline-buffer-size nil 'l))
+                                       (when powerline-display-mule-info
+                                         (powerline-raw mode-line-mule-info nil 'l))
+                                       (powerline-buffer-id nil 'l)
+                                       (when (and (boundp 'which-func-mode) which-func-mode)
+                                         (powerline-raw which-func-format nil 'l))
+                                       (powerline-raw " ")
+                                       (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                         (powerline-raw erc-modified-channels-object face1 'l))
+                                       (powerline-major-mode face1 'l)
+                                       (powerline-process face1)
+                                       (powerline-minor-modes face1 'l)
+                                       (powerline-narrow face1 'l)
+                                       (powerline-raw " " face1)
+                                       (powerline-vc face2 'r)
+                                       (when (bound-and-true-p nyan-mode)
+                                         (powerline-raw (list (nyan-create)) face2 'l))))
+                            (rhs (list (powerline-raw global-mode-string face2 'r)
+                                       (unless window-system
+                                         (powerline-raw (char-to-string #xe0a1) face1 'l))
+                                       (when powerline-display-hud
+                                         (powerline-hud face2 face1)))))
+                       (concat (powerline-render lhs)
+                               (powerline-fill face2 (powerline-width rhs))
+                               (powerline-render rhs)))))))
