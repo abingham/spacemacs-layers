185f3bb6812e7fb4c2f746d5b65ba28d854616bd
Austin Bingham <austin@sixty-north.com>
2016-03-18 13:36:13 +0000

    Trying out vim bindings.

--- abingham/keybindings.el
+++ abingham/keybindings.el
@@ -1,25 +1,25 @@
-(global-set-key [(ctrl x) (i)] 'windmove-up)
-(global-set-key [(ctrl x) (m)] 'windmove-down)
-(global-set-key [(ctrl x) (j)] 'windmove-left)
-(global-set-key [(ctrl x) (l)] 'windmove-right)
+;; (global-set-key [(ctrl x) (i)] 'windmove-up)
+;; (global-set-key [(ctrl x) (m)] 'windmove-down)
+;; (global-set-key [(ctrl x) (j)] 'windmove-left)
+;; (global-set-key [(ctrl x) (l)] 'windmove-right)
 
-(global-set-key (kbd "<f5>") 'magit-status)
+;; (global-set-key (kbd "<f5>") 'magit-status)
 
-(global-set-key [(ctrl h) (f)] 'helm-apropos)
-(global-set-key [(ctrl h) (v)] 'helm-apropos)
-(global-set-key [(ctrl c) (h)] 'helm-command-prefix)
-(global-set-key [(ctrl c) (ctrl f)] 'helm-find-files)
-(global-set-key [(ctrl x) (b)] 'helm-mini)
-;; (global-set-key [(ctrl c) (h) (o)] 'helm-occur)
-(global-set-key [(meta x)] 'helm-M-x)
-(global-set-key [(ctrl x) (return)] 'helm-my-buffers)
+;; (global-set-key [(ctrl h) (f)] 'helm-apropos)
+;; (global-set-key [(ctrl h) (v)] 'helm-apropos)
+;; (global-set-key [(ctrl c) (h)] 'helm-command-prefix)
+;; (global-set-key [(ctrl c) (ctrl f)] 'helm-find-files)
+;; (global-set-key [(ctrl x) (b)] 'helm-mini)
+;; ;; (global-set-key [(ctrl c) (h) (o)] 'helm-occur)
+;; (global-set-key [(meta x)] 'helm-M-x)
+;; (global-set-key [(ctrl x) (return)] 'helm-my-buffers)
 
-(global-set-key [(meta /)] 'company-complete)
+;; (global-set-key [(meta /)] 'company-complete)
 
-(global-set-key [(control l)] 'line-to-top-of-window)
+;; (global-set-key [(control l)] 'line-to-top-of-window)
 
-(global-set-key [(ctrl .)] 'comment-or-uncomment-region)
+;; (global-set-key [(ctrl .)] 'comment-or-uncomment-region)
 
-(global-set-key [(ctrl x) (y) (i)] 'yas-insert-snippet)
+;; (global-set-key [(ctrl x) (y) (i)] 'yas-insert-snippet)
 
-(global-set-key [(ctrl shift c) (ctrl shift l)] 'mc/mark-next-like-this)
+;; (global-set-key [(ctrl shift c) (ctrl shift l)] 'mc/mark-next-like-this)
