c695f586e354d125f6e621457351d1572d211032
Austin Bingham <austin@sixty-north.com>
2016-03-19 16:24:43 +0100

    Added f5 for magit-status.

--- abingham/keybindings.el
+++ abingham/keybindings.el
@@ -3,7 +3,7 @@
 ;; (global-set-key [(ctrl x) (j)] 'windmove-left)
 ;; (global-set-key [(ctrl x) (l)] 'windmove-right)
 
-;; (global-set-key (kbd "<f5>") 'magit-status)
+(global-set-key (kbd "<f5>") 'magit-status)
 
 ;; (global-set-key [(ctrl h) (f)] 'helm-apropos)
 ;; (global-set-key [(ctrl h) (v)] 'helm-apropos)
